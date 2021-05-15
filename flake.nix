cb46c15b9c21562ebd2ae9487b254af9820597f0
John Rinehart <john.rinehart@ardanlabs.com>
2021-05-15 11:54:15 +0300

    feat: define system configuration using a flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      flake = true;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    agenix.url = "github:ryantm/agenix";
+  };
+
+  outputs = inputs:
+    let
+      home-manager-config = {
+        config = {
+          home-manager = {
+            useUserPackages = true;
+            users.john = ./john.nix;
+          };
+        };
+      };
+    in
+    {
+      nixosConfigurations.nixos = with inputs.nixpkgs; lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          inputs.agenix.nixosModules.age
+          inputs.home-manager.nixosModules.home-manager
+          home-manager-config
+        ];
+
+        specialArgs = { inherit (inputs) nixpkgs agenix; };
+      };
+    };
+}
